3d1dcbcd28581c71c4bc9e63ee3ecc9ce8a0d5f5
Danaid Sinani <twiiooyt@gmail.com>
2024-12-21 03:40:42 -0500

    gave up on github pages deployed on vercel instead

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -13,7 +13,7 @@ export default function Navigation() {
         <nav className="w-full backdrop-blur-sm fixed top-0 left-0">
             <div className="container mx-auto px-4 py-4 flex flex-col sm:flex-row items-center justify-between">
                 <Link href="/" className="text-2xl font-bold text-black mb-4 sm:mb-0">
-                    Danaid Sinani
+                    personal website
                 </Link>
                 <div className="flex space-x-8">
                     {/* Navigation links */}
