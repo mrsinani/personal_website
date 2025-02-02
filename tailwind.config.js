4a17f21ce228132a57c49f28b8ed96fed1b8da3b
Danaid Sinani <twiiooyt@gmail.com>
2025-02-02 01:30:59 -0500

    gradient doesnt work need to just use the same one

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,17 @@ module.exports = {
 		extend: {
 			colors: {
 				aurora: {
-					blue: '#60a5fa',
-					indigo: '#a5b4fc',
-					violet: '#ddd6fe'
-				}
-			},
+				  blue: '#60a5fa',    // Existing blue
+				  indigo: '#a5b4fc',  // Existing indigo
+				  violet: '#ddd6fe',  // Existing violet
+				  sage300: '#8FF7A7', // Light Green Mint
+				  sage400: '#52E09B', // Vibrant Teal Green
+				  sage500: '#1FC978', // Strong Emerald Green
+				  sage600: '#00A964', // Deep Jade
+				  sage700: '#008754', // Dark Forest Green
+				  sage800: '#006A42', // Almost Black-Green
+				  sage900: '#004D2C', // Deepest Dark Teal
+				}},
 			animation: {
 				aurora: 'aurora 20s linear infinite'
 			},
